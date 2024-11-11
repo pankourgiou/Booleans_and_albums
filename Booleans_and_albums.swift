b6f2b84992c1803906dd19d64118efb216f929cc
L <60981745+pankourgiou@users.noreply.github.com>
2024-11-11 21:34:09 +0200

    Create Booleans_and_albums.swift

---
+++ Booleans_and_albums.swift
@@ -0,0 +1,47 @@
+import Foundation
+
+let  zeroonezerooneonezerozeroone = true // Boolean variable
+let  Ayreon = true                                  
+
+// Simple conditional check
+if zeroonezerooneonezerozeroone {
+    print("This is my new amore...it is one of the coolest albums I've ever heard-->Ayreon")
+} else {
+    print("Ayreon are cool")
+}
+let  disconnected = true // Boolean variable
+let  parallels = true                                  
+
+// Simple conditional check
+if disconnected {
+    print("I still love Kevin Moore's little keys and don't forget we are all One!!")
+} else {
+    print("parallels is cool")
+}
+let  APleasantshadeofgrey = true // Boolean variable
+let  Longdaygoodnight = true                                  
+
+// Simple conditional check
+if APleasantshadeofgrey {
+    print("A pleasant shade of grey is Iconic but still I love disconnected more")
+} else {
+    print("let's have fun")
+}
+let  printtwoone = true // Boolean variable
+let  printzero = true                                  
+
+// Simple conditional check
+if printtwoone {
+    print("21")
+} else {
+    print("0")
+}
+let  printonetwo = true // Boolean variable
+let  zero = true                                  
+
+// Simple conditional check
+if  printonetwo {
+    print("12")
+} else {
+    print("0")
+}
